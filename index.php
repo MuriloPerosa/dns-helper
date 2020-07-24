d1237fbf7594bf466f1e190dd00bae6f2ce482c7
Murilo Perosa <158117@upf.br>
2020-07-24 00:02:45 -0300

    git ignore

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php
-
-use Classes\Dns\Domain;
-use Symfony\Component\VarDumper\VarDumper;
-
 require __DIR__ . '/vendor/autoload.php';
-
-
-$domain = new Domain('aç~o.xyz');
-
-$domains = [
-    'dsamurilo.com',
-    'http://teste2.com',
-    'https://teste3.com',
-    'https://www.teste3.com',
-];
-foreach ($domains as $d) {
-   VarDumper::dump($domain->sanitizeDomain($d));
-}
